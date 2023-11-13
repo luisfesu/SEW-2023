00b5bfba370b9af4b2f156036c67b979f825906e
Luis A. Fernandez Suarez <luisfesu2000@gmail.com>
2023-11-13 14:55:11 +0100

    Clase 13/11 - Primeras clases de javascript añadid

---
+++ js/memoria.js
@@ -0,0 +1,31 @@
+"use strict"
+class Memoria {
+    constructor() {
+        this.elements = {
+            HTML: {
+                element:"HTML5",
+                source:"https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
+            },
+            CSS3: {
+                element:"CSS3",
+                source:"https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
+            },
+            JS: {
+                element:"JS",
+                source:"https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg"
+            },
+            PHP: {
+                element:"PHP",
+                source:"https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
+            },
+            SVG: {
+                element:"SVG",
+                source:"https://upload.wikimedia.org/wikipedia/commons/4/4f/SVG_Logo.svg"
+            },
+            W3C: {
+                element:"W3C",
+                source:"https://upload.wikimedia.org/wikipedia/commons/5/5e/W3C_icon.svg"
+            },
+        }
+    }
+}
